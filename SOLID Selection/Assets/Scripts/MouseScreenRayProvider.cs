a79ed9b7c4e8f6fa358d6aa8eaf2323ce01f6924
SISI <schwermc@my.erau.edu>
2025-11-05 11:50:09 -0700

    Change mouse ray

--- SOLID Selection/Assets/Scripts/MouseScreenRayProvider.cs
+++ SOLID Selection/Assets/Scripts/MouseScreenRayProvider.cs
@@ -4,6 +4,7 @@ public class MouseScreenRayProvider : MonoBehaviour, IRayProvider
 {
     public Ray CreateRay()
     {
-        return Camera.main.ScreenPointToRay(Input.mousePosition);
+        return Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        // return Camera.main.ScreenPointToRay(Input.mousePosition);
     }
 }
